6a93aaa168bf8b88c4f824409878f2ed51f6314a
MagusX <brianddoan@gmail.com>
2022-09-22 23:57:20 +0700

    Add get week dates util

--- src/utils/DateTimeUtils.ts
+++ src/utils/DateTimeUtils.ts
@@ -1,5 +1,32 @@
+import { numberPadLeft } from './StringUtils';
+
 export function getWeekDayName(weekDay: number | undefined): string {
   if (weekDay === undefined) return '';
   const weekDayNames: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   return weekDayNames[weekDay - 1];
+}
+
+export function getWeekDates(today: Date): Date[] {
+  const week: Date[] = []; 
+  today.setDate((today.getDate() - today.getDay() + 1));
+  for (let i = 0; i < 7; i++) {
+      week.push(
+          new Date(today)
+      ); 
+      today.setDate(today.getDate() + 1);
+  }
+  return week; 
+}
+
+export function getWeekDatesFormatted(today: Date): string[] {
+  const week: string[] = []; 
+  today.setDate((today.getDate() - today.getDay() + 1));
+  for (let i = 0; i < 7; i++) {
+    const _date: Date = new Date(today);
+    week.push(
+        `${_date.getFullYear()}-${numberPadLeft(_date.getMonth() + 1)}-${numberPadLeft(_date.getDate())}`
+    ); 
+    today.setDate(today.getDate() + 1);
+  }
+  return week; 
 }
