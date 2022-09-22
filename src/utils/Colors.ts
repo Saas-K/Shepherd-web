1e57c079de6486ae805e9360dfaeea40e8d087fc
MagusX <brianddoan@gmail.com>
2022-09-23 00:37:09 +0700

    Add color consts

---
+++ src/utils/Colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  grey1: '#84817a',
+  blue1: '#60a3bc',
+  green1: '#33d9b2',
+  purple1: '#706fd3',
+  orange1: '#ff793f',
+  green2: '#78e08f'
+}
+
+export const colorsList = [
+  colors.blue1, 
+  colors.green2,
+  colors.green1, 
+  colors.purple1,
+  colors.orange1, 
+  colors.grey1, 
+]
